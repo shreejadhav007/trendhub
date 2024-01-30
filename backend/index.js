c61bcf4352fb9984cfa81cbe67e99b8f6450b75b
shreejadhav007 <104477778+shreejadhav007@users.noreply.github.com>
2024-01-30 11:58:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const port = 3001;
 main()
 app.use(express.json());
 app.use(cors({
-     origin:"https://scheduliadaily.netlify.app",
+     origin:"https://trendii.netlify.app/",
     methods:["GET","POST","PUT","DELETE"]
 }));
 
